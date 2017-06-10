808126b4d6590f40c72c037231acebc6f23367d2
Gavin Kyte <gpkyte@gmail.com>
2017-06-10 00:13:28 -0400

    Move to sqlite
    Store results in sqlite table.
    Created function for repeat use.

--- scrape-test.py
+++ scrape-test.py
@@ -1,49 +1,70 @@
 import urllib2
 from bs4 import BeautifulSoup
+import pandas as pd
+import sqlite3
+
+def scrape_sale(sale):
+    link = "https://bid.bidfta.com/cgi-bin/mnprint.cgi?{}".format(sale)
+    page = urllib2.urlopen(link)
+    soup = BeautifulSoup(page, 'lxml')
+
+    catalog = soup.find('table', { 'id': 'DataTable' })
+
+    conn = sqlite3.connect("fta.sqlite")
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS items (\
+        location text, \
+        id text, \
+        description text, \
+        info text, \
+        UNIQUE(id) ON CONFLICT REPLACE)")
+
+    args = []
+    for row in catalog.findAll("tr")[1:]:
+        cells = row.findAll('td')
+        itemId = cells[0].find(text=True).strip(".")
+        description = None
+        info = None
+        location = None
+
+        #itemLink = "https://bid.bidfta.com/cgi-bin/mnlist.cgi?{}/{}" \
+        #    .format(sale, itemCode)
+
+
+        print(itemId)
+        details = cells[1].findAll('b')
+        for elem in details:
+            tag = elem.find(text=True)
+            content = elem.next_sibling
+            if (not content):
+                continue
+            if (content[0] == ":"):
+                content = content[2:]
+            print("    {}: {}".format(tag, content))
+
+            if (tag == "Item Description"):
+                description = content
+            elif (tag == "Additional Info"):
+                info = content
+            elif (tag == "Item Location"):
+                location = content
+            else:
+                pass
+
+            if (description and info and location):
+                break
+
+        args.append((
+            location,
+            itemId,
+            description,
+            info))
+
+    cursor.executemany(
+        "INSERT INTO items(location, id, description, info) \
+        VALUES(?,?,?,?)", args)
+    conn.commit()
+
 
 sale = "schoolii1969"
-link = "https://bid.bidfta.com/cgi-bin/mnprint.cgi?{}".format(sale)
-page = urllib2.urlopen(link)
-soup = BeautifulSoup(page, 'lxml')
-
-catalog = soup.find('table', { 'id': 'DataTable' })
-
-A=[] # Code
-B=[] # Description
-C=[] # Additional Info
-D=[] # Location
-
-for row in catalog.findAll("tr")[1:]:
-    cells = row.findAll('td')
-    itemCode = cells[0].find(text=True).strip(".")
-    itemLink = "https://bid.bidfta.com/cgi-bin/mnlist.cgi?{}/{}" \
-        .format(sale, itemCode)
-    A.append(itemCode)
-    print(itemCode)
-
-    description = cells[1].findAll('b')
-    for elem in description:
-        tag = elem.find(text=True)
-        content = elem.next_sibling
-        if (not content):
-            continue
-
-        if (content[0] == ":"):
-            content = content[2:]
-        print("    {}: {}".format(tag, content))
-
-        if (tag == "Item Description"):
-            B.append(content)
-        elif (tag == "Additional Info"):
-            C.append(content)
-        elif (tag == "Item Location"):
-            D.append(content)
-        else:
-            pass
-
-import pandas as pd
-df=pd.DataFrame(A,columns=['Code'])
-df['Description']=B
-df['Additional Info']=C
-df['Location']=D
-print(df)
+scrape_sale(sale)
